6f895376e16a51949fd4ee88b2dc3c2a926ad2c6
Ezequiel Ramos <ezequielmr94@gmail.com>
2026-03-12 13:30:50 -0300

    feature/addingIosSdk

---
+++ ios/openlprsdk/openlprsdk/openlprsdk.swift
@@ -0,0 +1,16 @@
+//
+//  openlprsdk.swift
+//  openlprsdk
+//
+//  Created by Ezequiel Ramos on 11/03/26.
+//
+
+public class OpenLprSdk {
+
+    public init() {}
+
+    public func hello() -> String {
+        return "OpenLPR SDK OK"
+    }
+
+}
